ba4ba7c82bac283e76985d280f46c5842161d4c8
김건우 <rjswjddn@gmail.com>
2025-06-13 11:13:17 +0900

    test: 테스트 어플리케이션 가짜키 주입

--- src/test/java/com/team573/gongguri/GongguriApplicationTests.java
+++ src/test/java/com/team573/gongguri/GongguriApplicationTests.java
@@ -2,8 +2,10 @@ package com.team573.gongguri;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "UNIV_CERTIFICATION_API_KEY=test-key")
 class GongguriApplicationTests {
 
     @Test
