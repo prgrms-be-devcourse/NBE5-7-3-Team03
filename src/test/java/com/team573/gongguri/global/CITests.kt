e233f424bede594892c300c52b70f82b92577152
Ji-minhyeok <gmh8257@gmail.com>
2025-06-16 11:54:01 +0900

    chore: Testcontainers 환경 추상화 및 분리

--- src/test/java/com/team573/gongguri/global/CITests.kt
+++ src/test/java/com/team573/gongguri/global/CITests.kt
@@ -1,10 +1,9 @@
 package com.team573.gongguri.global
 
-import com.team573.gongguri.GongguriApplicationTests.Companion.mongoContainer
-import com.team573.gongguri.GongguriApplicationTests.Companion.mysqlContainer
+import com.team573.gongguri.integration.AbstractIntegrationTest
 import org.junit.jupiter.api.Test
 
-class CITests {
+class CITests : AbstractIntegrationTest(){
     @Test
     fun `컨테이너 정상 기동 확인`() {
         println("MySQL running at: ${mysqlContainer.jdbcUrl}")
---
+++ src/test/java/com/team573/gongguri/integration/AbstractIntegrationTest.kt
@@ -0,0 +1,35 @@
+package com.team573.gongguri.integration
+
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+abstract class AbstractIntegrationTest {
+    companion object {
+        @Container
+        val mysqlContainer = MySQLContainer<Nothing>("mysql:8.0.33").apply {
+            withDatabaseName("testdb")
+            withUsername("testuser")
+            withPassword("testpass")
+            withExposedPorts(3306)
+            start()
+            System.setProperty("spring.datasource.url", jdbcUrl)
+            System.setProperty("spring.datasource.username", username)
+            System.setProperty("spring.datasource.password", password)
+        }
+
+        @Container
+        val mongoContainer = MongoDBContainer("mongo:6.0.6").apply {
+            start()
+            val containerIpAddress = this.host
+            val port = this.getMappedPort(27017)
+
+            System.setProperty(
+                "spring.data.mongodb.uri",
+                "mongodb://$containerIpAddress:$port/testdb"
+            )
+        }
+    }
+}
