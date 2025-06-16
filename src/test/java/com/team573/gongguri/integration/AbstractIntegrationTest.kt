10a089f2d9ac54598cac9358d8218b84b396406d
김건우 <rjswjddn@gmail.com>
2025-06-16 17:22:44 +0900

    feat: 통합테스트 수정

--- src/test/java/com/team573/gongguri/integration/AbstractIntegrationTest.kt
+++ src/test/java/com/team573/gongguri/integration/AbstractIntegrationTest.kt
@@ -17,24 +17,25 @@ abstract class AbstractIntegrationTest {
             withDatabaseName("testdb")
             withUsername("testuser")
             withPassword("testpass")
-            withExposedPorts(3306)
-            start()
-            System.setProperty("spring.datasource.url", jdbcUrl)
-            System.setProperty("spring.datasource.username", username)
-            System.setProperty("spring.datasource.password", password)
+//            withExposedPorts(3306)
+//            start()
+//            System.setProperty("spring.datasource.url", jdbcUrl)
+//            System.setProperty("spring.datasource.username", username)
+//            System.setProperty("spring.datasource.password", password)
         }
 
         @Container
-        val mongoContainer = MongoDBContainer("mongo:6.0.6").apply {
-            start()
-            val containerIpAddress = this.host
-            val port = this.getMappedPort(27017)
-
-            System.setProperty(
-                "spring.data.mongodb.uri",
-                "mongodb://$containerIpAddress:$port/testdb"
-            )
-        }
+        val mongoContainer = MongoDBContainer("mongo:6.0.6")
+//        val mongoContainer = MongoDBContainer("mongo:6.0.6").apply {
+//            start()
+//            val containerIpAddress = this.host
+//            val port = this.getMappedPort(27017)
+//
+//            System.setProperty(
+//                "spring.data.mongodb.uri",
+//                "mongodb://$containerIpAddress:$port/testdb"
+//            )
+//        }
 
         @JvmStatic
         @DynamicPropertySource
