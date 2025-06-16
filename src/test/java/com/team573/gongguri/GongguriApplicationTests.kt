cefd6685f14ced3c9ac5a24d9293e228ff8ffff1
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2025-06-16 12:09:01 +0900

    chore: 테스트용 yml 분리 및 Testcontainers 기반 CI 환경 구축 (#30)
    * Create CI.yml
    
    * chore: 테스트용 yml 분리 및 Testcontainers 기반 CI 환경 구축
    
    * chore: Testcontainers 환경 추상화 및 분리

--- src/test/java/com/team573/gongguri/GongguriApplicationTests.kt
+++ src/test/java/com/team573/gongguri/GongguriApplicationTests.kt
@@ -1,12 +1,15 @@
 package com.team573.gongguri
 
+import com.team573.gongguri.integration.AbstractIntegrationTest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
 @TestPropertySource(properties = ["UNIV_CERTIFICATION_API_KEY=test-key"])
-class GongguriApplicationTests {
+@ActiveProfiles("test")
+class GongguriApplicationTests : AbstractIntegrationTest() {
 	@Test
 	fun contextLoads() {
 	}
