e73a6dbeddaa6d8af4a6978f7b4d9dabae2cf4f2
Ji-minhyeok <gmh8257@gmail.com>
2025-06-16 16:01:34 +0900

    chore: Testcontainers 환경 컨테이너의 불필요한 메서드 제거

--- src/test/java/com/team573/gongguri/integration/AbstractIntegrationTest.kt
+++ src/test/java/com/team573/gongguri/integration/AbstractIntegrationTest.kt
@@ -15,24 +15,10 @@ abstract class AbstractIntegrationTest {
             withDatabaseName("testdb")
             withUsername("testuser")
             withPassword("testpass")
-            withExposedPorts(3306)
-            start()
-            System.setProperty("spring.datasource.url", jdbcUrl)
-            System.setProperty("spring.datasource.username", username)
-            System.setProperty("spring.datasource.password", password)
         }
 
         @Container
-        val mongoContainer = MongoDBContainer("mongo:6.0.6").apply {
-            start()
-            val containerIpAddress = this.host
-            val port = this.getMappedPort(27017)
-
-            System.setProperty(
-                "spring.data.mongodb.uri",
-                "mongodb://$containerIpAddress:$port/testdb"
-            )
-        }
+        val mongoContainer = MongoDBContainer("mongo:6.0.6")
 
         @JvmStatic
         @DynamicPropertySource
