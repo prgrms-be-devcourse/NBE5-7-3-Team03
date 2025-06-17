15b791fd95d73ccc05729b94dd1c441d8c4e5adf
김건우 <rjswjddn@gmail.com>
2025-06-17 12:31:21 +0900

    test: 채팅 관련 공동구매 테스트

---
+++ src/test/java/com/team573/gongguri/domain/grouppurchase/integration/GroupPurchaseIntegrationTests.kt
@@ -0,0 +1,91 @@
+package com.team573.gongguri.domain.grouppurchase.integration
+
+import com.team573.gongguri.integration.AbstractIntegrationTest
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.patch
+
+class GroupPurchaseIntegrationTests: AbstractIntegrationTest() {
+
+	@Test
+	fun `채팅방 리스트 조회`() {
+		val size = 10
+
+		mockMvc.get("/api/group-purchases/chat") {
+			contentType = MediaType.APPLICATION_JSON
+			param("progressStatus", "ALL")
+			param("size", size.toString())
+			with(user(userDetails))
+		}.andExpect {
+			status { isOk() }
+		}.andDo {
+			print()
+		}
+	}
+
+	@Test
+	fun `결제 상태로 변환`() {
+		val groupPurchaseId: Long = 10
+		val participantsId: Long = 12
+
+		mockMvc.patch("/api/group-purchases/$groupPurchaseId/participants/$participantsId/confirm") {
+			contentType = MediaType.APPLICATION_JSON
+			with(user(userDetails))
+			with(csrf())
+		}.andExpect {
+			status { isNoContent() }
+		}.andDo {
+			print()
+		}
+	}
+
+	@Test
+	fun `참가자 강퇴`() {
+		val groupPurchaseId: Long = 10
+		val participantsId: Long = 12
+
+		mockMvc.patch("/api/group-purchases/$groupPurchaseId/participants/$participantsId/cancel") {
+			contentType = MediaType.APPLICATION_JSON
+			with(user(userDetails))
+			with(csrf())
+		}.andExpect {
+			status { isNoContent() }
+		}.andDo {
+			print()
+		}
+	}
+
+	@Test
+	fun `참가자 목록을 조회`() {
+		val groupPurchaseId: Long = 10
+		val size = 10
+
+		val firstResult = mockMvc.get("/api/group-purchases/$groupPurchaseId/participants") {
+			contentType = MediaType.APPLICATION_JSON
+			param("size", size.toString())
+			with(user(userDetails))
+		}.andExpect {
+			status { isOk() }
+		}.andDo {
+			print()
+		}.andReturn()
+	}
+
+	@Test
+	fun `공동 구매를 조회`() {
+		val size = 10
+
+		mockMvc.get("/api/group-purchases") {
+			contentType = MediaType.APPLICATION_JSON
+			param("size", size.toString())
+			with(user(userDetails))
+		}.andExpect {
+			status { isOk() }
+		}.andDo {
+			print()
+		}
+	}
+}
